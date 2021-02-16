602b9565fe35b1e0641878a75514763686ecb763
Quentin <quentinjeanferet@gmail.com>
2021-02-17 00:54:13 +0100

    add files

---
+++ src/App.js
@@ -0,0 +1,278 @@
+import React from 'react';
+
+const projectName = '25-5-clock';
+
+//accurate interval : allow to add a break at the end of the clock runner 
+const accurateInterval = function (fn, time) {
+  var cancel, nextAt, timeout, wrapper;
+  //nextAt : Length of the break between 2 run 
+    nextAt = new Date().getTime() + time;
+    timeout = null;
+    wrapper = function () {
+      nextAt += time;
+      timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+      return fn();
+    };
+  cancel = function () {
+    return clearTimeout(timeout);
+  };
+  timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+  return {
+    cancel: cancel
+  };
+};
+
+// COMPONENTS
+
+//-------------------------------------------------------
+//   Timer Length Control : 1 - Button Arrow Down
+//                          2 - Value of the Break Timer
+//                          3 - Button Arrow Up
+//-------------------------------------------------------
+class TimerLengthControl extends React.Component {
+  render() {
+    return (
+      <div className="length-control">
+        <div id={this.props.titleID}>{this.props.title}</div>        
+        <button
+          className="btn-level"
+          id={this.props.minID}
+          onClick={this.props.onClick}
+          value="-"
+        >
+          <i className="fa fa-arrow-down fa-2x" />
+        </button>
+        <div className="btn-level" id={this.props.lengthID}>
+          {this.props.length}
+        </div>
+        <button
+          className="btn-level"
+          id={this.props.addID}
+          onClick={this.props.onClick}
+          value="+"
+        >
+          <i className="fa fa-arrow-up fa-2x" />
+        </button>
+      </div>
+    );
+  }
+}
+//-------------------------------------------------------
+//   Timer
+//-------------------------------------------------------
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    //Variable
+    this.state = {
+      brkLength: 5,
+      seshLength: 25,
+      timerState: 'stopped',
+      timerType: 'Session',
+      timer: 1500,
+      intervalID: '',
+      alarmColor: { color: 'white' }
+    };
+    //Methods
+    this.setBrkLength = this.setBrkLength.bind(this);
+    this.setSeshLength = this.setSeshLength.bind(this);
+    this.lengthControl = this.lengthControl.bind(this);
+    this.timerControl = this.timerControl.bind(this);
+    this.beginCountDown = this.beginCountDown.bind(this);
+    this.decrementTimer = this.decrementTimer.bind(this);
+    this.phaseControl = this.phaseControl.bind(this);
+    this.warning = this.warning.bind(this);
+    this.buzzer = this.buzzer.bind(this);
+    this.switchTimer = this.switchTimer.bind(this);
+    this.clockify = this.clockify.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  //Functions
+  //Define the value of the timer
+  setBrkLength(e) {
+    this.lengthControl(
+      'brkLength',
+      e.currentTarget.value,
+      this.state.brkLength,
+      'Session'
+    );
+  }
+  //Define the value of the break
+  setSeshLength(e) {
+    this.lengthControl(
+      'seshLength',
+      e.currentTarget.value,
+      this.state.seshLength,
+      'Break'
+    );
+  }
+  //Define the limit of the timer and the break timer
+  lengthControl(stateToChange, sign, currentLength, timerType) {
+    if (this.state.timerState === 'running') {
+      return;
+    }
+    if (this.state.timerType === timerType) {
+      if (sign === '-' && currentLength !== 1) {
+        this.setState({ [stateToChange]: currentLength - 1 });
+      } else if (sign === '+' && currentLength !== 60) {
+        this.setState({ [stateToChange]: currentLength + 1 });
+      }
+    } else if (sign === '-' && currentLength !== 1) {
+      this.setState({
+        [stateToChange]: currentLength - 1,
+        timer: currentLength * 60 - 60
+      });
+    } else if (sign === '+' && currentLength !== 60) {
+      this.setState({
+        [stateToChange]: currentLength + 1,
+        timer: currentLength * 60 + 60
+      });
+    }
+  }
+  //Define the running time 
+  timerControl() {
+    if (this.state.timerState === 'stopped') {
+      this.beginCountDown();
+      this.setState({ timerState: 'running' });
+    } else {
+      this.setState({ timerState: 'stopped' });
+      if (this.state.intervalID) {
+        this.state.intervalID.cancel();
+      }
+    }
+  }
+  //Launch the decrement phase when the button play is clicked
+  beginCountDown() {
+    this.setState({
+      intervalID: accurateInterval(() => {
+        this.decrementTimer();
+        this.phaseControl();
+      }, 1000)
+    });
+  }
+  //define the decrement timer 
+  decrementTimer() {
+    this.setState({ timer: this.state.timer - 1 });
+  }
+  //Switch from the break timer to the working timer
+  phaseControl() {
+    let timer = this.state.timer;
+    this.warning(timer);
+    this.buzzer(timer);
+    if (timer < 0) {
+      if (this.state.intervalID) {
+        this.state.intervalID.cancel();
+      }
+      if (this.state.timerType === 'Session') {
+        this.beginCountDown();
+        this.switchTimer(this.state.brkLength * 60, 'Break');
+      } else {
+        this.beginCountDown();
+        this.switchTimer(this.state.seshLength * 60, 'Session');
+      }
+    }
+  }
+  //Change the color of the timer when the timer is below 60 secondes
+  warning(_timer) {
+    if (_timer < 61) {
+      this.setState({ alarmColor: { color: '#a50d0d' } });
+    } else {
+      this.setState({ alarmColor: { color: 'white' } });
+    }
+  }
+  //If the timer is equals to 0, launch the audio song
+  buzzer(_timer) {
+    if (_timer === 0) {
+      this.audioBeep.play();
+    }
+  }
+  switchTimer(num, str) {
+    this.setState({
+      timer: num,
+      timerType: str,
+      alarmColor: { color: 'white' }
+    });
+  }
+  //Adapt the clock to print the minutes and the seconds
+  clockify() {
+    let minutes = Math.floor(this.state.timer / 60);
+    let seconds = this.state.timer - minutes * 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return minutes + ':' + seconds;
+  }
+  //Define the reset button
+  reset() {
+    this.setState({
+      brkLength: 5,
+      seshLength: 25,
+      timerState: 'stopped',
+      timerType: 'Session',
+      timer: 1500,
+      intervalID: '',
+      alarmColor: { color: 'white' }
+    });
+    if (this.state.intervalID) {
+      this.state.intervalID.cancel();
+    }
+    this.audioBeep.pause();
+    this.audioBeep.currentTime = 0;
+  }
+  render() {
+    return (
+      <div>
+        <div className="main-title">25 + 5 Clock</div>
+        <TimerLengthControl
+          addID="break-increment"
+          length={this.state.brkLength}
+          lengthID="break-length"
+          minID="break-decrement"
+          onClick={this.setBrkLength}
+          title="Break Length"
+          titleID="break-label"
+        />
+        <TimerLengthControl
+          addID="session-increment"
+          length={this.state.seshLength}
+          lengthID="session-length"
+          minID="session-decrement"
+          onClick={this.setSeshLength}
+          title="Session Length"
+          titleID="session-label"
+        />
+        <div className="timer" style={this.state.alarmColor}>
+          <div className="timer-wrapper">
+            <div id="timer-label">{this.state.timerType}</div>
+            <div id="time-left">{this.clockify()}</div>
+          </div>
+        </div>
+        <div className="timer-control">
+          <button id="start_stop" onClick={this.timerControl}>
+            <i className="fa fa-play fa-2x" />
+            <i className="fa fa-pause fa-2x" />
+          </button>
+          <button id="reset" onClick={this.reset}>
+            <i className="fa fa-refresh fa-2x" />
+          </button>
+        </div>
+        <div className="author">
+          {' '}
+          Designed with the help of <br />
+          <a href="https://codepen.io/freeCodeCamp/full/XpKrrW" target="_blank">
+            FreeCodeCamp
+          </a>
+        </div>
+        <audio
+          id="beep"
+          preload="auto"
+          ref={(audio) => {
+            this.audioBeep = audio;
+          }}
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        />
+      </div>
+    );
+  }
+}
+
+export default Timer;
